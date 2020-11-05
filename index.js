f67fee30b78b9fb05de02a0758aff4cbbffcd031
Jue Wang <joywang912@hotmail.com>
2020-11-05 15:48:28 +1300

    Calculate and print total revenue and total expenses

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 const fs = require("fs");
+const sumBy = require("lodash/sumBy");
+
+/**
+ * Function to calculate sum of total_value in the list,
+ * and filter by category if exist
+ * @param {Array} list
+ * @param {String} category
+ */
+const calcTotalValue = (list, category = "") => {
+  const calcList = category
+    ? list.filter((el) => el.account_category === category)
+    : list;
+
+  return sumBy(calcList, "total_value");
+};
 
 /**
  * Read finanical list from data.json
@@ -11,6 +26,10 @@ const financialList = dataObj && dataObj.data;
  * Check if finanical list exist and print results
  */
 if (financialList) {
+  // Print Revenue
+  console.log(`Revenue: $${calcTotalValue(financialList, "revenue")}`);
+  // Print Expenses
+  console.log(`Expenses: $${calcTotalValue(financialList, "expense")}`);
 } else {
   console.error("Finanical list not found.");
 }
